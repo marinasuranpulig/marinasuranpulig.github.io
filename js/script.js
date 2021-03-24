6695b6007bea6ee9054c48430189836b303fbf08
Ivana <ivana@Ivanas-MBP.local>
2021-03-24 21:50:28 +0100

    correct image paths

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ function add_swap_img_handlers() {
     let swap_imgs = [];
     for (let i = 0; i < imgs.length; i++) {
         if(imgs[i].className === "swapable") {
-            console.log("swapable images: ", imgs[i].src)
+            //console.log("swapable images: ", imgs[i].src)
             imgs[i].addEventListener("mouseenter", image_focus_handler);
             imgs[i].addEventListener("mouseleave", image_focus_handler);
             imgs[i].addEventListener("touchstart", image_focus_handler);
@@ -22,8 +22,7 @@ function add_swap_img_handlers() {
 function swap_images(src_path) {
     let src_path_spl = src_path.split(".");
     let src_extension = src_path_spl.pop();
-    let src_base = src_path_spl.join("");
-
+    let src_base = src_path_spl.join(".");
     let current_nr = src_base.charAt(src_base.length - 1);
     let next_nr = (Number(current_nr) % 2) + 1;
 
